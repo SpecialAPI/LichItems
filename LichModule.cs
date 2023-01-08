ab5f77ea2f7d0df3a6324ab3b66fe14ee26c9255
SpecialAPI <pavlov.andrei.d@gmail.com>
2023-01-08 15:12:53 +0300

    fixed the mod breaking when starting the game in a language other than english

--- LichModule.cs
+++ LichModule.cs
@@ -11,7 +11,7 @@ using BepInEx;
 
 namespace LichItems
 {
-    [BepInPlugin("spapi.etg.lichitems", "Lich Items", "1.0.1")]
+    [BepInPlugin("spapi.etg.lichitems", "Lich Items", "1.0.3")]
     [BepInDependency(ETGModMainBehaviour.GUID)]
     public class LichModule : BaseUnityPlugin
     {
